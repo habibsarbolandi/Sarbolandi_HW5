b1d9b4598a830bcb04fbf1f04ca79bad6bf1c666
habibsarbolandi <jsarbola@asu.edu>
2017-10-25 09:45:28 -0700

    all work until the navigation controller addition to medTVC

---
+++ Sarbolandi_HW5/Med.swift
@@ -0,0 +1,22 @@
+//
+//  Med.swift
+//  Sarbolandi_HW5
+//
+//  Created by Habib Sarbolandi on 10/24/17.
+//  Copyright © 2017 Habib Sarbolandi. All rights reserved.
+//
+
+import Foundation
+
+class Med {
+    let name: String
+    let dosage: String
+    let daily: String
+    
+    init(name: String, dosage: String, daily: String) {
+        self.name = name
+        self.dosage = dosage
+        self.daily = daily
+    }
+    
+}
---
+++ Sarbolandi_HW5/medTVC.swift
@@ -0,0 +1,122 @@
+//
+//  medTVC.swift
+//  Sarbolandi_HW5
+//
+//  Created by Habib Sarbolandi on 10/24/17.
+//  Copyright © 2017 Habib Sarbolandi. All rights reserved.
+//
+
+import UIKit
+
+protocol CellSelectedDelegate {
+    func read(med: Med)
+}
+
+class medTVC: UITableViewController {
+
+    var meds = [Med]()
+    let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: Selector(("addMed")))
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        self.navigationItem.rightBarButtonItem = addButton
+    }
+
+    func addMed() {
+        let alertName = UIAlertController(title: "Medication", message: "Type name of med", preferredStyle: .alert)
+        let confirmName = UIAlertAction(title: "Confirm", style: UIAlertActionStyle.default, handler: ({
+            (_) in
+            
+        }))
+       // let alertDosage = UIAlertController(title: "Dosage", message: "Type the dosage of med (ie 125 or 275)", preferredStyle: .alert)
+       // let alertDaily = UIAlertController(title: "Daily", message: "Number of times taken per day (ie 1 or 5)", preferredStyle: .alert)
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return meds.count
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //TODO: react to user selecting row
+        //I want the detail view controller to update based on the row that I selected
+        
+        let selectedMed = meds[indexPath.row]
+        
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "medCell", for: indexPath)
+
+        // Configure the cell...
+        let currentMed = meds[indexPath.row]
+        cell.textLabel?.text = currentMed.name
+        cell.detailTextLabel?.text = "Dosage: \(currentMed.dosage)   Taken: \(currentMed.daily)x times per day"
+
+        return cell
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
